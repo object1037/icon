ff30c92c550140705ff22ca4abee5d2e9240e73d
object1037 <contact@object1037.dev>
2022-08-09 03:46:35 +0900

    change footer design

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ import Icon from './icon'
 export default function Footer() {
   return (
   <footer className="flex items-center bg-gray-100 dark:bg-gray-800 py-3 px-6 mt-auto">
-    <div className="flex flex-row items-center justify-between w-full max-w-5xl mx-auto">
-      <div className="flex flex-row flex-wrap space-x-2">
+    <div className="flex flex-col sm:flex-row items-center justify-between w-full max-w-5xl mx-auto">
+      <div className="flex flex-row space-x-2">
         <IconLink title='Homepage' link='https://object1037.dev' >
           <Icon colors={{
             Hair: 'none',
@@ -22,7 +22,7 @@ export default function Footer() {
           <FiGithub />
         </IconLink>
       </div>
-      <div className="text-gray-800 dark:text-gray-200 font-light text-xs">&copy; 2022 object1037</div>
+      <div className="text-gray-800 dark:text-gray-200 font-light text-xs p-4">&copy; 2022 object1037</div>
     </div>
   </footer>
   )
