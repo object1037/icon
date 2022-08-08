11c3f559870fcbaf8990b540516f9655c21a4e56
object1037 <contact@object1037.dev>
2022-08-08 21:07:30 +0900

    add icon component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Icon from '../components/icon'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+  const [hair, setHair] = useState('')
+  const [eye, setEye] = useState('')
+
   return (
     <>
     <Head>
@@ -10,7 +14,7 @@ const Home: NextPage = () => {
     </Head>
     <main>
       <div>
-        
+        <Icon width={256} height={256} hair={hair} eye={eye} />
       </div>
     </main>
     </>
