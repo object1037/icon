109eaf266ba1152feb66d5396b132bfec0d6ef5b
object1037 <contact@object1037.dev>
2022-08-09 14:03:22 +0900

    add color in dark mode

--- components/partPIcker.tsx
+++ components/partPIcker.tsx
@@ -10,8 +10,8 @@ const PartPicker = ({
   focused: boolean
 }) => {
   return (
-    <button onClick={setPart} className={`relative rounded-full w-9 h-9 border ${focused ? 'border-gray-700' : 'border-gray-400'}`} style={{ backgroundColor: `${color}` }} title={title} >
-      <div className={`absolute -bottom-4 inset-x-0 mx-auto w-1.5 h-1.5 rounded-full bg-gray-700 ${focused ? 'visible' : 'invisible'}`} />
+    <button onClick={setPart} className={`relative rounded-full w-9 h-9 border ${focused ? 'border-gray-700 dark:border-gray-200' : 'border-gray-400 dark:border-gray-500'}`} style={{ backgroundColor: `${color}` }} title={title} >
+      <div className={`absolute -bottom-4 inset-x-0 mx-auto w-1.5 h-1.5 rounded-full bg-gray-700 dark:bg-gray-200 ${focused ? 'visible' : 'invisible'}`} />
     </button>
   )
 }
