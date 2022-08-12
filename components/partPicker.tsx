e6a94c4d5f916a574ab9f0dcc9f02a8d4029c699
object1037 <contact@object1037.dev>
2022-08-12 18:39:03 +0900

    update partPicker background

--- components/partPicker.tsx
+++ components/partPicker.tsx
@@ -10,7 +10,8 @@ const PartPicker = ({
   focused: boolean
 }) => {
   return (
-    <button onClick={setPart} className={`relative rounded-full w-9 h-9 border ${focused ? 'border-gray-700 dark:border-gray-200' : 'border-gray-400 dark:border-gray-500'}`} style={{ backgroundColor: `${color}` }} title={title} >
+    <button onClick={setPart} className={`react-colorful__alpha-pointer relative rounded-full w-9 h-9 border ${focused ? 'border-gray-700 dark:border-gray-200' : 'border-gray-400 dark:border-gray-500'}`} title={title} >
+      <div className="react-colorful__pointer-fill" style={{ backgroundColor: `${color}` }} />
       <div className={`absolute -bottom-4 inset-x-0 mx-auto w-1.5 h-1.5 rounded-full bg-gray-700 dark:bg-gray-200 ${focused ? 'visible' : 'invisible'}`} />
     </button>
   )
