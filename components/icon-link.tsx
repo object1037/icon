eaaf9f47e06246f19d323598012b0bea2e445eb7
object1037 <contact@object1037.dev>
2022-11-11 16:26:31 +0900

    update packages

--- components/icon-link.tsx
+++ components/icon-link.tsx
@@ -1,19 +1,21 @@
-import Link from 'next/link'
-
 export default function IconLink({
   link,
   title,
   children,
 }: {
-  link: string,
-  title: string,
-  children: React.ReactNode,
+  link: string
+  title: string
+  children: React.ReactNode
 }) {
   return (
-    <Link href={link}>
-      <a target="_blank" rel="noopener noreferrer" title={title} className="p-4 hover:text-gray-400 dark:hover:text-gray-500 transition">
-        <p className="text-3xl">{children}</p>
-      </a>
-    </Link>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      title={title}
+      className="p-4 hover:text-gray-400 dark:hover:text-gray-500 transition"
+    >
+      <p className="text-3xl">{children}</p>
+    </a>
   )
 }
